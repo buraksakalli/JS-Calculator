8164e9206d42c1ca4e718220e3e4eb40b23cb359
mr.edizcelik@gmail.com <astroe.celik@gmail.com>
2017-03-09 12:41:12 +0100

    getting result from arthm function

--- script.js
+++ script.js
@@ -25,21 +25,37 @@ function compute(arr) {
 	//get rid of the "=" sign as the last element of the array
 	arr.pop(); 
 	const len = arr.length;
-
-	let arthm = arr.filter(a => isNaN(a));
 	console.log(arr);
-	console.log(arthm);
+
+	let a = arr.slice(0, len-2);
+	let result = arthm(a, arr[len-1], arr[len-2]);
+	console.log(result);
+	if (isNaN(result)) result = "Wrong input";
+	//console.log(isNaN(a));
 }
 
 function arthm(a, b, operator) {
+	if (isNaN(a)) {
+		console.log(a);
+		if (a.length > 3) {
+			let c = a.slice(0, a.length-2);
+			a = arthm(c, a[a.length-1], a[a.length-2]);
+		} else {
+			let c = a[0];
+			a = arthm(c, a[2], a[1]);
+		}
+
+	};
+	a = parseFloat(a); b = parseFloat(b);
 	switch(operator) {
-		case "+":
-			return a+b;
-		case "-":
-			return a-b;
-		case "x":
-			return a*b;
-		case "/":
-			return a/b;
+			case "+":
+				return a+b;
+			case "-":
+				return a-b;
+			case "x":
+				return a*b;
+			case "/":
+				return a/b;
 	}
+
 }
